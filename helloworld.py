d08768801d9d459422a1d0e06696c750e82f4517
MayThandarOo <mayforever2006@gmail.com>
2024-06-24 16:08:07 +0900

    add global variables

--- helloworld.py
+++ helloworld.py
@@ -4,3 +4,12 @@ if 5 > 2:print("Five is greater than two!")
 
 if 5 > 2:
     print("Five is greater than two!")
+    #This is a comment.    print("Five is greater than two!")
+#global variables
+
+x = "awesome"
+
+def myfunc():
+    print("Python is " + x)
+
+myfunc()
