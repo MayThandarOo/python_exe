ce2b35f7b396c43f2c7630a4a40142df4960e900
MayThandarOo <mayforever2006@gmail.com>
2024-06-25 10:06:34 +0900

    demo_mysql

--- helloworld.py
+++ helloworld.py
@@ -1,15 +1,34 @@
-print("Hello world!")
+# print("Hello world!")
 
-if 5 > 2:print("Five is greater than two!")
+# if 5 > 2:print("Five is greater than two!")
 
-if 5 > 2:
-    print("Five is greater than two!")
-    #This is a comment.    print("Five is greater than two!")
-#global variables
+# if 5 > 2:
+#     print("Five is greater than two!")
+#     #This is a comment.    print("Five is greater than two!")
+# #global variables
 
-x = "awesome"
+# x = "awesome"
 
-def myfunc():
-    print("Python is " + x)
+# def myfunc():
+#     global x 
+#     x = "fantastic"
+#     print("Python is " + x)
 
-myfunc()
+# myfunc()
+
+# print("Python is " + x)
+
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+
+a1 = 200
+b1 = 33
+c1 = 500
+if a1 > b1 and c1 > a1:
+    print("Both conditions are true")
